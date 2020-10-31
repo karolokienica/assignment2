37cadfa7cab2c98a78d5955fdc30f04c44965b48
karolokienica <karolokienica@gmail.com>
2020-10-31 13:01:36 +0100

    basic form of assignment2 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
+class App extends Component {
+  state = { text: "" };
+
+  changeTextHandler = (event) => {
+    this.setState(
+      {text: event.target.value}
+      );
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={this.changeTextHandler}/>
+      <label>{this.state.text}</label>
     </div>
-  );
+  )
+  };
 }
 
 export default App;
